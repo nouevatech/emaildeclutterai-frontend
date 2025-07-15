0a0c64c07aeef9d0420e7779daf6188e64f530ca
kofoworola_shonuyi <skofoworola3@gmail.com>
2025-07-15 19:37:40 +0100

    trying to fix merge issues

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,12 +1,12 @@
 import { Demo } from "../components/Demo";
 import { HeroSection } from "../components/HeroSection";
-import { HowItWorks } from "../components/HowItWorks";
+// import { HowItWorks } from "../components/HowItWorks";
 
 export default function Home() {
   return (
     <div>
       <HeroSection />
-      <HowItworks />
+      {/* <HowItWorks /> */}
       <Demo />
     </div>
   );
