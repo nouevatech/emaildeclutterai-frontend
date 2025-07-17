b71e260611c7b712a1a3b390599e7899ea9bf955
roselinemo <roselinemorenikejic@gmail.com>
2025-07-17 17:20:33 +0100

    Update baseurl

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://emaildeclutterai-backend.onrender.com/gmail",
+  baseURL: "https://emaildeclutterai-backend.onrender.com/gmail/fetch",
   withCredentials: true,
 });
 
